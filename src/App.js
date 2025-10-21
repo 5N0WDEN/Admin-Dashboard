5688c08a346a768d11fb79723447e615065d6eaa
5N0WDEN <utkarshmhatre.7777@gmail.com>
2025-10-21 21:25:06 +0530

    setting up the project

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import { Route, BrowserRouter, Routes } from 'react-router-dom';
+import "bootstrap/dist/css/bootstrap.min.css";
+import './App.css';
+import Dashboard from './pages/Dashboard/';
+import Header from './components/Header';
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path='/' exact={true} element={<Dashboard/>} />
+        <Route path='/dashboard' exact={true} element={<Dashboard/>} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/components/Header/index.js
@@ -0,0 +1,16 @@
+
+const Header = () => {
+    return (
+        <>
+            <header>
+                <div className="container-fluid">
+                    <button className="btn btn-danger">
+                        Header
+                    </button>
+                </div>
+            </header>
+        </>
+    )
+}
+
+export default Header;
